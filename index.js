628d296bb8e87d9cfe8078695830125985e50b99
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-12-10 11:15:02 +0530

    icon name corection

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+// JavaScript
 const socials = [
-    { name: 'Facebook', icon: './res/facebook.png' },
-    { name: 'Instagram', icon: './res/instagram.png' },
-    { name: 'Whatsapp', icon: './res/whatsapp.png' },
-    { name: 'Message', icon: './res/inbox.png' },
-    { name: 'Phone', icon: './res/telephone.png' },
-  ];
-  
-  const footer = document.getElementById('footer');
-  
-  socials.forEach(social => {
-    const div = document.createElement('div');
-    div.innerHTML = `<img src="${social.icon}" alt="${social.name} icon"> <span>${social.name}</span>`;
-    div.className = 'soc';
-    footer.appendChild(div);
-  });
+  { name: 'Facebook',  icon: './res/facebook.png', url: 'https://www.facebook.com' },
+  { name: 'Instagram', icon: './res/instagram.png', url: 'https://www.instagram.com' },
+  { name: 'Whatsapp', icon: './res/whatsapp.png', url: 'https://www.whatsapp.com' },
+  { name: 'Message', icon: './res/inbox.png', url: 'https://www.example.com/message' },
+  { name: 'Phone', icon: './res/telephone.png', url: 'tel:+1234567890' },
+];
+
+const footer = document.getElementById('footer');
+
+socials.forEach(social => {
+  const div = document.createElement('div');
+  div.innerHTML = `<a class="socialbtn" href="${social.url}" target="_blank"><img src="${social.icon}" alt="${social.name} icon"> <span class="iconname">${social.name}</span></a>`;
+  div.className = 'soc';
+  footer.appendChild(div);
+});
