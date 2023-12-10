dad01f2e392f584771e5e4f76bbabb560f923c08
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-12-10 11:06:21 +0530

    added icons

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("I am a cutiee pieeee!!!")
+const socials = [
+    { name: 'Facebook', icon: './res/facebook.png' },
+    { name: 'Instagram', icon: './res/instagram.png' },
+    { name: 'Whatsapp', icon: './res/whatsapp.png' },
+    { name: 'Message', icon: './res/inbox.png' },
+    { name: 'Phone', icon: './res/telephone.png' },
+  ];
+  
+  const footer = document.getElementById('footer');
+  
+  socials.forEach(social => {
+    const div = document.createElement('div');
+    div.innerHTML = `<img src="${social.icon}" alt="${social.name} icon"> <span>${social.name}</span>`;
+    div.className = 'soc';
+    footer.appendChild(div);
+  });
