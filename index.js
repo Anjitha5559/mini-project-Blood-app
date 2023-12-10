d4493d89723e7fc214e9076b71ae019844186cda
SreejithKSGupta <sreejithksgupta2255@gmail.com>
2023-12-10 14:51:38 +0530

    added logo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const socials = [
   { name: 'Phone', icon: './res/telephone.png', url: 'tel:+1234567890' },
 ];
 
-const footer = document.getElementById('footer');
+const footer = document.getElementById('iconrow');
 
 socials.forEach(social => {
   const div = document.createElement('div');
@@ -15,3 +15,30 @@ socials.forEach(social => {
   div.className = 'soc';
   footer.appendChild(div);
 });
+
+
+const quotesAndFacts = [
+  "Blood donation is the best donation",
+  "Every two seconds someone in the U.S. needs blood",
+  "A single donation can potentially help more than one patient",
+  "One donation can save up to three lives",
+  "Blood is the most precious gift that anyone can give to another person",
+];
+
+const quotesDiv = document.getElementById("quotes");
+let currentQuoteIndex = 0;
+
+function updateQuote() {
+  quotesDiv.classList.add('fade-out');setTimeout(() => {
+    quotesDiv.textContent = quotesAndFacts[currentQuoteIndex];
+    quotesDiv.classList.remove('fade-out');
+    quotesDiv.classList.add('fade-in'); setTimeout(() => {
+      quotesDiv.classList.remove('fade-in');
+    }, 1000);
+
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotesAndFacts.length;
+  }, 1000);
+}
+
+updateQuote();
+setInterval(updateQuote, 4000);
